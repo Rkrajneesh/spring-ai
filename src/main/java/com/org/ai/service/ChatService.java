862853f1bffe0489988e2317beb04f8ad9998891
rkrajneesh <rkrajneesh91@gmail.com>
2026-03-30 17:52:37 +0530

    using prompt

--- src/main/java/com/org/ai/service/ChatService.java
+++ src/main/java/com/org/ai/service/ChatService.java
@@ -1,7 +1,9 @@
 package com.org.ai.service;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.messages.AssistantMessage;
 import org.springframework.ai.chat.messages.SystemMessage;
+import org.springframework.ai.chat.messages.UserMessage;
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.stereotype.Service;
 
@@ -24,7 +26,9 @@ public class ChatService {
 //        return chatClient.prompt().system("You are an experienced spring boot trainer. Always explain step by step in bullet points.").user(message).call().content();
 
 //        Prompt with message list.
-        Prompt prompt = new Prompt(List.of(new SystemMessage("You are friendly travel guide, Always suggest 3 attractions and 1 food items.")));
+        Prompt prompt = new Prompt(List.of(new SystemMessage("You are friendly travel guide, Always suggest 3 attractions and 1 food items."),
+                new UserMessage("Plan my day in Rome."),
+                new AssistantMessage("Morning: Visit the Collosseum\\nAfternoon: Explore the Vatican Museum\\nEvening: See the Trevi Fountain. \\nFood: gelato.")));
         return chatClient.prompt(prompt).user(message).call().content();
     }
 }
